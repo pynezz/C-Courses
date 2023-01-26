f4f41f31d1d853222180a5eac855bfc664c3592a
pynezz <k@pro-x.x>
2023-01-26 20:01:34 +0100

    Oppgave 3

--- Leksjon 3/oppgaver.c
+++ Leksjon 3/oppgaver.c
@@ -8,40 +8,90 @@
 #include <string.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <ctype.h>
 
+// void err_n_die(const char *fmt, ...);
 
-
-// err_n_die:
-// Print error message but do not exit.
-void err_n_die(const char *fmt, ...) {
-    int errno_save;
-    va_list ap;
-
-    errno_save = errno; // value caller might want printed
-
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, " \r ");
-    fflush(stderr);
-
-    if (errno_save != 0) {
-        fprintf(stderr, "(errno = %d) : %s \r ", errno_save, strerror(errno_save));
-        fprintf(stderr, " \r ");
-        fflush(stderr);
-    }
-
-    va_end(ap);
-    return;
-}
-
+int oppgave_1(); 
 
 // run with gcc -Wall -o oppgaver oppgaver.c
 
-void oppgave_1() {
+int main(int argc, char **argv) 
+{
+    int num;
+
+    char input[] = {*argv[1]};
+    // printf("10 is equal to %d bytes, and argv[1] is %d bytes", sizeof(10), sizeof(input[0]));
+    if ((isxdigit(input[0]), 2) && (input[0] != 48))
+    { // 48 = ASCII 0
+        printf("input is digit\n");
+        if (check_valid_input(argv[1]) >= 0)
+        {
+            num = atoi(argv[1]);
+            int isEven = num % 2 == 0 ? 1 : 0;
+            char odd[14] = "odd\n";
+            char even[15] = "even\n";
+            printf("Number is %s", isEven == 1 ? even : odd);
+        } else printf("please provide a non-zero integer as argument!\n");
+        // return isEven ? "Number is even" : "Number is odd";
+    }
+    else
+        printf("please provide a non-zero integer as argument!\n");
+
+    int x, y, z, w;
+
+    x = 10;
+    y = 9;
+    z = 8;
+    w = 7;
+
+    if ((x == y++) && (z == ++x))
+    {
+        w = 100;
+        printf("This is obviously true"); // false
+    }
+
+    z = ++w + y++; // z is probably going to be z(8) = 7 + 9 = 16
+    // Correct answer is 18 - not sure how to test what get's executed first
+
+    printf("z = %d\n", z);
+    printf("Next!\n");
+
+    if (argc < 2)
+    {
+        // err_n_die("No arguments given. Printing all exercises. \r\n");
+        oppgave_1();
+    }
+
+    if (strcmp(argv[1], "1") == 0)
+    {
+        oppgave_1();
+        // err_n_die("Done. \r ");
+    }
+
+    return 0;
+}
+
+int check_valid_input(char input[]) {
+    for (int i = 0; i < sizeof(input) / sizeof(char); i++) {
+        if (!isdigit(input[i])) {
+            return -1;
+        }
+    }
+    return 0;
+
+}
+
+int oppgave_1()
+{
+
+    // sizeof int:
+    printf("Size of int:            %ld bytes \n", sizeof(int));
+
     // sizeof float:
     printf("Size of float:          %ld bytes \r\n", sizeof(float));
 
-    // sizeof double: 
+    // sizeof double:
     printf("Size of double:         %ld bytes \r\n", sizeof(double));
 
     // sizeof long:
@@ -64,22 +114,29 @@ void oppgave_1() {
     
     // sizeof int array[20]:
     printf("Size of int array[20]:  %ld bytes \r\n", sizeof(int[20]));
-    
-    return;
+
+    return 0;
 }
 
+// err_n_die:
+// Print error message but do not exit.
+// void err_n_die(const char *fmt, ...) {
+//     int errno_save;
+//     va_list ap;
 
-int main(int argc, char **argv)
-{      
-    if (argc < 2) {
-        err_n_die("No arguments given. Printing all exercises. \r\n");
-        oppgave_1();
-    }
+//     errno_save = errno; // value caller might want printed
 
-    if (strcmp(argv[1], "1") == 0) {
-        oppgave_1();
-        err_n_die("Done. \r ");
-    }
+//     va_start(ap, fmt);
+//     vfprintf(stderr, fmt, ap);
+//     fprintf(stderr, " \r ");
+//     fflush(stderr);
 
-    return 0;    
-}
+//     if (errno_save != 0) {
+//         fprintf(stderr, "(errno = %d) : %s \r ", errno_save, strerror(errno_save));
+//         fprintf(stderr, " \r ");
+//         fflush(stderr);
+//     }
+
+//     va_end(ap);
+//     return;
+// }
