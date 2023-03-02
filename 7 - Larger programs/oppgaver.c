c66a3b0ff92088179092c2d2aa110e8f7c830fee
Kevin <kevin@pop-os.localdomain>
2023-03-02 15:42:04 +0100

    task 1 done

--- 7 - Larger programs/oppgaver.c
+++ 7 - Larger programs/oppgaver.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int oppgave_1();
 int oppgave_2();
 
-int main(int *argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	if (argc < 2) {
-		printf("Usage: %s <task>", *argv[0]);
+		printf("Usage: %s <task>\n\r\n", argv[0]);
+		return 0;
 	}
 
 	char argument;
@@ -14,7 +16,10 @@ int main(int *argc, char *argv[]) {
 	for (int i = 1; i < argc; i++) {
 		switch (atoi(argv[i])) {
 			case 1:
-				oppgave_1();
+				for (int i = 0; i < 3; i++) {
+				
+					oppgave_1();
+				}
 				break;
 			case 2:
 				oppgave_2();
@@ -27,7 +32,18 @@ int main(int *argc, char *argv[]) {
 
 // Write a function, that can print how many times it has been called so far
 // without using a global variable
-
 int oppgave_1() {
-	
+	static int iCalls;
+
+	iCalls++;
+
+	printf("Calls: %d\n", iCalls); 
+
+	return 0;
+}
+
+int oppgave_2() {
+	printf("Not implemented yet\n");
+
+	return 0;
 }
